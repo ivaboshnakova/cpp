8b64cfb478aac174c966e2d9b061e3b0f1d26469
Iva Boshnakova <ivaboshnakova@abv.bg>
2020-11-13 19:53:30 +0200

    new folder

---
+++ functions/task1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+ double sum(const int a, const int b){
+ 	return a+b;
+ }
+ 
+ int main (){
+ 	int num1, num2;
+ 	cout<<"num1 ="<<" ";
+ 	cin>>num1;
+ 	cout<<"num2 ="<<" ";
+ 	cin>>num2;
+ 	cout<<sum(num1,num2);
+ 	return 0;
+ }
+ 
+ 
+
